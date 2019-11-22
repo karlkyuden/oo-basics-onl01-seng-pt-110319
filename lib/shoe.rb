5150f4d8d1fce69347e4083a52b97e2314ee876c
Karl Jerrenny Ojeda <karlojeda@yahoo.com>
2019-11-22 18:17:44 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -8,12 +8,13 @@ class Shoe
   attr_accessor :brand, :color, :size, :material, :condition
   
   def cobble
-    condition()
+
     puts "Your shoe is as good as new!"
+    condition("new")
   end
   
   def condition=(status)
-    @condition = "new"
+    @condition = status
   end
   
   
